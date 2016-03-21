ee986cefad4d79556ddee0384a61af6772285753
Timon Sliepenbeek <Timon Sliepenbeek>
2016-03-21 20:12:32 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Pasen 2016</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
+  </head>
+
+  <body>
+
+    <div class="header">
+      <img class="banner" src="img/banner.png" alt="" />
+      <img class="date" src="img/label.png" alt="" />
+    </div>
+
+    <div class="row">
+      <div class="col-md-6 col-xs-12 col-sm-6">
+        <h1>Voor de kids - Vindt het voorjaar !</h1>
+        <p>
+          In het winkelgebied van Spekholzerheide zitten op de zaterdag voor pasen vier "levende" voorjaarsfiguren verstopt, namelijk een kip, een narcissenmevrouw, een vos, een konijn en een kuiken. Als je bij ieder figuur een stempel verzamelt, kun je de volle stempelkaart bij de Paashaas - die je op het Carboonplein vindt - omruilen voor een leuk cadeau.
+        </p>
+      </div>
+      <div class="col-md-6 col-xs-12 col-sm-6">
+        <h1>Paaspakket winnen</h1>
+        <p>
+          Ongeveer 30 ondernemers uit Spekholzerheide hebben spulletjes gesponsord waar leuke paaspakketten van zijn gemaakt (onderaan de pagina vindt u een opsomming van deze ondernemers). Iedereen die op Paaszaterdag tussen 12:00 en 16:00 uur bij de Paashaas op het Carboonplein een kassabon met zijn of haar naam en telefoonnummer inlevert, maakt kans op één van de pakketjes. Hoe meer kassabonnen u inlevert, hoe meer kans u maakt!
+        </p>
+      </div>
+      <div class="col-md-6 col-xs-12 col-sm-6">
+        <h1>Voor iedereen - Gratis narcissen</h1>
+        <p>
+          Op de Akerstraat en het Carboonplein worden tussen 12:00 en 16:00 uur gratis narcissen aan bezoekers uitgedeeld. Gewoon als een kleine attentie omdat het wij hopelijk snel weer van mooi voorjaarsweer kunnen genieten.
+        </p>
+      </div>
+      <div class="col-md-6 col-xs-12 col-sm-6">
+        <h1>Sponsorende ondernemers</h1>
+        <p>
+          Op Paaszaterdag worden - door een professionele fotograaf - ook mooie foto's gemaakt. Na afloop worden de foto's op de <a href="http://carboon.nl/facebook">Facebookpagina van Carboon</a> geplaatst. De mensen op de foto met de meeste Likes winnen een mooi cadeaupakket.
+        </p>
+      </div>
+      <div class="col-md-12 col-xs-12 col-sm-12">
+        <h1>Met dank aan</h1>
+        <p>
+          Alle ondernemers in de Akerstraat en aan het Carboonplein hebben een bijdrage geleverd in de realisatie van de Paasactie. De volgende ondernemers - bijna iedereen - hebben daarbovenop nog een extra bijdrage geleverd.<br>
+        </p>
+        <p><br>
+          Bakker Brouns,
+          Barbershop,
+          BE Scooters,
+          Bloemen en meer,
+          Blokker,
+          Brasserie Carboon,
+          Brood & Banketbakkerij Kengen,
+          Crazy Fashion,
+          De Hakkenbar,
+          De Tapijtenhal,
+          Devils-Road-Tattoos,
+          DIVA Kleding Nailstyling & accessiores,
+          Eran Floor V.O.F,
+          Franssen Verhuur,
+          Hendriks Bloemen,
+          Het Opscheertje,
+          Jan Linders,
+          Jumbo,
+          Kapsalon De Hoofdzaak,
+          Marhaba Abdoel,
+          Naaimachine Totaal,
+          Reloaded,
+          René Janssen Kapper,
+          Slagerij Schröder,
+          Spekhof Optiek,
+          Toma Cycles,
+          Traiteur Ritt,
+          Umberto's,
+          Uw Auto!,
+          Zapatos
+        </p>
+      </div>
+    </div>
+
+  </body>
+
+</html>
